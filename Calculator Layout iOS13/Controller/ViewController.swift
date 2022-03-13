e0cbc0a9d51b7b9fe9e5984e5a7cc8d7d7ff4837
Junsoo Park <matthiaspark0514@gmail.com>
2022-03-13 14:59:53 -0700

    code factoring, guard let or if let?

--- Calculator Layout iOS13/Controller/ViewController.swift
+++ Calculator Layout iOS13/Controller/ViewController.swift
@@ -36,12 +36,11 @@ class ViewController: UIViewController {
         calculator.setNumber(displayValue)
         
         if let calcMethod = sender.currentTitle {
-            
-            
-            guard let result = calculator.calculate(symbol: calcMethod) else {
-                fatalError("The result of calculation is nil")
+        
+            if let result = calculator.calculate(symbol: calcMethod) {
+                displayValue = result
             }
-            displayValue = result
+            
         }
     }
     
